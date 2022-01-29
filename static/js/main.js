3f00bab81153024471ac7be96bd58aab30727d76
YASSlNE <yessinemahmoud@yahoo.fr>
2022-01-29 15:18:16 +0100

    added download option

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,9 @@
+function hide_div(){
+    $('#mp3').css('display','none');
+    return true;
+}
 function get_links(){
     $('div#mp3').html($('#mp3').text()==''? '':$('#mp3').text());
     $('#mp3').css('display','block');
+    return true;
 }
